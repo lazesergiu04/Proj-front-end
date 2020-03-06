9a2bf35151265d6f6572d22c65feec1d417492d5
lazesergiu <lazesergiu04@gmail.com>
2020-03-06 21:18:15 +0000

    display the books in div but bookmark is not working

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ const bookTitle= document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const searchResult = document.getElementById("searchResult");
 const  bookshelf =document.getElementById("content");
-const icon = document.createElement("div");
-
+const bookItems = document.getElementById("bookItem");
 const bookmark = document.getElementById("bookmark");
 let booksResult =[];
-let test =[];
+let bookElements =[];
 
 
 
-
+bookmark.addEventListener("click", ()=>{
+    bookmark.innerText="Click"
+})
 
 addButton.addEventListener('click',  () =>{
     addButton.style.display ="none";
    searchForm.style.display ="inline-block";
    newBook.style.display= "none";
-
-
 })
 
 cancelButton.addEventListener("click", ()=>{
@@ -38,48 +37,55 @@ cancelButton.addEventListener("click", ()=>{
 })
 
 let apiRequest = new XMLHttpRequest();
-
-apiRequest.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        const result = JSON.parse(this.responseText);
-        for (let x= 0; x < result.items.length; x++){
-            booksResult.push(result.items[x])
-        }
-        console.log(booksResult);
-
-        console.log(bookElements())
-
-    }
-}
-    function bookElements() {
-        for (let i = 0; i < booksResult.length; i++) {
-            let title = booksResult[i]["volumeInfo"]["title"];
-            let author = booksResult[i]["volumeInfo"]["authors"];
-            let url = booksResult[i]["volumeInfo"]["imageLinks"]["thumbnail"];
-            let book ={title,author,url}
-
-        }
-
-
-        }
-
-    function bookFormat(title,author, url){
-        let bookForm= "<div id='resultBook'>" +
-         "<h4 id='resultTitle'>"+title+"</h4>"+
-         "<h5 id='resultAuthor'>"+ author+"</h5>"+
-         "<img src="+url+">" +
-         "</div>"
-     return bookForm;
- }
-
-
- searchForm.addEventListener('submit', ($event) => {
+searchForm.addEventListener('submit', ($event) => {
     $event.preventDefault();
     const authorInput = bookAuthor.value;
     const  titleInput= bookTitle.value;
     apiRequest.open('GET', "https://www.googleapis.com/books/v1/volumes?q=" +titleInput + authorInput );
     apiRequest.send();
 })
+
+
+apiRequest.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        const result = JSON.parse(this.responseText);
+        for (let x = 0; x < result.items.length; x++) {
+            let res = result.items[x];
+            booksResult.push(res);
+        }
+        for (let i = 0; i < booksResult.length; i++) {
+            let title = booksResult[i]["volumeInfo"]["title"];
+            let author = booksResult[i]["volumeInfo"]["authors"];
+            let url = booksResult[i]["volumeInfo"]["imageLinks"]["thumbnail"];
+            const book = {title, author, url}
+            bookElements.push(book)
+
+            searchResult.innerHTML += bookFormat(title, author, url);
+
+            searchResult.appendChild(bookmark);
+
+
+        }
+    }
+
+}
+
+function bookFormat(title,author, url){
+        let bookForm="<div>"+
+         "<h4 id='resultTitle'>"+title+"</h4>"+
+         "<h5 id='resultAuthor'>"+ author+"</h5>"+
+         "<img src="+url+">"+"</div>"
+    return bookForm;
+ }
+
+
+
+
+
+
+
+
+
 
 
 
