6968df1086097ebbd239cf67c071e68d9cd5fbd7
lazesergiu <lazesergiu04@gmail.com>
2020-03-09 10:45:24 +0000

    bookmark is working and stored sessionStorage

--- script.js
+++ script.js
@@ -1,149 +1,85 @@
+const addButton = document.getElementById('btn-add');
+const cancelButton = document.getElementById('btn-cancel');
+const searchForm = document.getElementById('searchForm');
+const newBook = document.getElementById('newBook');
+const searchButton = document.getElementById('btn-search');
+const bookTitle = document.getElementById('bookTitle');
+const bookAuthor = document.getElementById('bookAuthor');
+const searchResult = document.getElementById('searchResult');
+const bookshelf = document.getElementById('content');
 
-const addButton = document.getElementById("btn-add");
-const  cancelButton= document.getElementById("btn-cancel");
-const searchForm = document.getElementById("searchForm");
-const  newBook = document.getElementById("newBook");
-const searchButton=  document.getElementById("btn-search");
-const bookTitle= document.getElementById("bookTitle");
-const bookAuthor = document.getElementById("bookAuthor");
-const searchResult = document.getElementById("searchResult");
-const  bookshelf =document.getElementById("content");
-const bookItems = document.getElementById("bookItem");
-const bookmark = document.getElementById("bookmark");
-let booksResult =[];
-let bookElements =[];
+let booksResult = [];
+let bookElements = [];
 
+addButton.addEventListener('click', () => {
+  addButton.style.display = 'none';
+  searchForm.style.display = 'inline-block';
+  newBook.style.display = 'none';
+});
 
-
-bookmark.addEventListener("click", ()=>{
-    bookmark.innerText="Click"
-})
-
-addButton.addEventListener('click',  () =>{
-    addButton.style.display ="none";
-   searchForm.style.display ="inline-block";
-   newBook.style.display= "none";
-})
-
-cancelButton.addEventListener("click", ()=>{
-
-    searchResult.style.display = "none";
-    addButton.style.display = "inline-block";
-    searchForm.style.display ="none";
-    newBook.style.display ="inline-block";
-    location.reload();
-
-
-})
+cancelButton.addEventListener('click', () => {
+  searchResult.style.display = 'none';
+  addButton.style.display = 'inline-block';
+  searchForm.style.display = 'none';
+  newBook.style.display = 'inline-block';
+  location.reload();
+});
 
 let apiRequest = new XMLHttpRequest();
-searchForm.addEventListener('submit', ($event) => {
-    $event.preventDefault();
-    const authorInput = bookAuthor.value;
-    const  titleInput= bookTitle.value;
-    apiRequest.open('GET', "https://www.googleapis.com/books/v1/volumes?q=" +titleInput + authorInput );
-    apiRequest.send();
-})
-
+searchForm.addEventListener('submit', $event => {
+  $event.preventDefault();
+  const authorInput = bookAuthor.value;
+  const titleInput = bookTitle.value;
+  apiRequest.open(
+    'GET',
+    'https://www.googleapis.com/books/v1/volumes?q=' + titleInput + authorInput
+  );
+  apiRequest.send();
+});
 
 apiRequest.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        const result = JSON.parse(this.responseText);
-        for (let x = 0; x < result.items.length; x++) {
-            let res = result.items[x];
-            booksResult = [res];
-            console.log(result)
-        }
-        for (let i = 0; i < booksResult.length; i++) {
-            let title = booksResult["volumeInfo"]["title"];
-            let author = booksResult["volumeInfo"]["authors"];
-            let url = booksResult["volumeInfo"]["imageLinks"]["thumbnail"];
-            const book = {title, author, url}
-            bookElements.push(book)
-
-            searchResult.innerHTML += bookFormat(title, author, url);
-
-            searchResult.appendChild(bookmark);
-
-
-        }
+  if (this.readyState === 4 && this.status === 200) {
+    const result = JSON.parse(this.responseText);
+    for (let x = 0; x < result.items.length; x++) {
+      let res = result.items[x];
+      booksResult.push(res);
     }
+    console.log(booksResult);
+    for (let i = 0; i < booksResult.length; i++) {
+      const currentBook = booksResult[i];
+      const bookId = currentBook.id;
+      const title = currentBook.volumeInfo.title;
+      const author = currentBook.volumeInfo.authors;
+      const url = currentBook.volumeInfo.imageLinks.thumbnail;
+      const book = bookFormat(bookId, title, author, url);
+      bookElements.push(book);
+    }
+    searchResult.innerHTML += bookElements;
+  }
+};
+
+function bookmarkBook(bookId) {
+  const currentBook = booksResult.filter(book => book.id === bookId);
+  sessionStorage.setItem(bookId, JSON.stringify(currentBook));
+  alert(`${currentBook[0].volumeInfo.title} Bookmarked!`);
 
 }
 
-function bookFormat(title,author, url){
-        let bookForm="<div>"+
-         "<h4 id='resultTitle'>"+title+"</h4>"+
-         "<h5 id='resultAuthor'>"+ author+"</h5>"+
-         "<img src="+url+">"+"</div>"
-    return bookForm;
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function bookFormat(bookId, title, author, url) {
+  let bookForm =
+    '<div>' +
+    '<button type="button" class="fa fa-bookmark" onclick="bookmarkBook(\'' +
+    bookId +
+    '\')"></button>;' +
+    "<h4 id='resultTitle'>" +
+    title +
+    '</h4>' +
+    "<h5 id='resultAuthor'>" +
+    author +
+    '</h5>' +
+    '<img src=' +
+    url +
+    ' />' +
+    '</div>';
+  return bookForm;
+}
