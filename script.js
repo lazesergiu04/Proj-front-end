92e0971623597303e3c1ba0a1979a510924babe1
lazesergiu <lazesergiu04@gmail.com>
2020-03-13 00:15:11 +0000

    add icon and design(before validations)

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let bookElements = [];
 
 addButton.addEventListener('click', () => {
   addButton.style.display = 'none';
-  searchForm.style.display = 'inline-block';
+  searchForm.style.display = 'block';
   newBook.style.display = 'none';
 });
 
@@ -50,31 +50,42 @@ apiRequest.onreadystatechange = function() {
       const bookId = currentBook.id;
       const title = currentBook.volumeInfo.title;
       const author = currentBook.volumeInfo.authors;
-      const url = currentBook.volumeInfo.imageLinks.thumbnail;
+      const url = availablePicture(currentBook);
       const book = bookFormat(bookId, title, author, url);
       bookElements.push(book);
     }
     searchResult.innerHTML += bookElements;
   }
 };
+function availablePicture(currentBook) {
+  if (currentBook.contains(currentBook.volumeInfo.imageLink) ) {
+    "css/unavailable-resized.png"} else { currentBook["volumeInfo"]["imageLinks"]["thumbnail"]}
+
+}
+
 
 function bookmarkBook(bookId) {
   const currentBook = booksResult.filter(book => book.id === bookId);
   sessionStorage.setItem(bookId,JSON.stringify( currentBook));
   alert(`${currentBook[0].volumeInfo.title} Bookmarked!`);
+
   sessionStorage.getItem(bookId);
   const savedTitle= currentBook[0].volumeInfo.title;
   const savedAuthor = currentBook[0].volumeInfo.authors;
   const savedUrl = currentBook[0].volumeInfo.imageLinks.thumbnail;
+  const savedBook = savedBookFormat(bookId,savedTitle,savedAuthor,savedUrl);
+
+bookshelf.innerHTML +=savedBook;
 
 
-  bookshelf.innerHTML +=
-     '<div id="savedResult" >' +'<button type="button" class="fa fa-trash" onclick="trashBook(\'' + bookId+ '\')"></button>'+
-     savedTitle +'<br>'+ savedAuthor + '<br>' +
-     '<img src='+savedUrl+'/>'
+}
+function savedBookFormat(bookId, savedTitle, savedAuthor, savedUrl){
+  let savedBookForm=  '<div id="savedResult" >' +'<button type="button" id="trash" class="fa fa-trash" onclick="trashBook(\'' + bookId+ '\')"></button>'+
+      savedTitle +'<br>'+ savedAuthor + '<br>' +
+      '<img src='+savedUrl+'/>'
 
-  +'</div>';
-
+      +'</div>';
+  return savedBookForm;
 }
 
 function trashBook(bookId) {
@@ -88,16 +99,10 @@ function trashBook(bookId) {
 function bookFormat(bookId, title, author, url) {
   let bookForm =
     '<div>' +
-    '<button type="button" class="fa fa-bookmark" onclick="bookmarkBook(\'' + bookId +
-    '\')"></button>' +
-    "<h4 id='resultTitle'>" +
-    title +
-    '</h4>' +
-    "<h5 id='resultAuthor'>" +
-    author +
-    '</h5>' +
-    '<img src=' +url +
-    ' />' +
+    '<button type="button"  id= "bookmark"class="fa fa-bookmark" onclick="bookmarkBook(\'' + bookId + '\')"></button>' +
+    "<h4 id='resultTitle'>" + title + '</h4>' +
+    "<h5 id='resultAuthor'>" + author + '</h5>' +
+    '<img src=' +url + ' />' +
     '</div>';
   return bookForm;
 }
